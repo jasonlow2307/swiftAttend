ebcda0f01d89236633279f20d6c1077c39732cf1
Jason <jason.lowjx@gmail.com>
2024-10-09 18:09:29 +0800

    Fixed session and aws accounts issues

--- common.py
+++ common.py
@@ -1,8 +1,10 @@
 import boto3
 
 # Initialize Boto3 client
-s3 = boto3.client('s3')
-dynamodb = boto3.client('dynamodb', region_name='ap-southeast-1')
-rekognition = boto3.client('rekognition', region_name='ap-southeast-1')
-cognito = boto3.client('cognito-idp', region_name='ap-southeast-1')
-lex_client = boto3.client('lexv2-runtime', region_name='ap-southeast-1')
+s = boto3.Session(profile_name='default')
+
+s3 = s.client('s3')
+dynamodb = s.client('dynamodb', region_name='ap-southeast-1')
+rekognition = s.client('rekognition', region_name='ap-southeast-1')
+cognito = s.client('cognito-idp', region_name='ap-southeast-1')
+lex_client = s.client('lexv2-runtime', region_name='ap-southeast-1')
