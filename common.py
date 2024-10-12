cf652fc13d3781dfaa2de4b0a1c0ad332f6ec03d
Jason <jason.lowjx@gmail.com>
2024-10-12 11:29:57 +0800

    Final EC2 test

--- common.py
+++ common.py
@@ -3,7 +3,7 @@ import boto3
 
 # Initialize Boto3 client
 s = boto3.Session(profile_name='default')
-
+'''
 s3 = s.client('s3')
 dynamodb = s.client('dynamodb', region_name='ap-southeast-1')
 rekognition = s.client('rekognition', region_name='ap-southeast-1')
@@ -16,4 +16,4 @@ s3 = boto3.client('s3')
 dynamodb = boto3.client('dynamodb', region_name='ap-southeast-1')
 rekognition = boto3.client('rekognition', region_name='ap-southeast-1')
 cognito = boto3.client('cognito-idp', region_name='ap-southeast-1')
-lex_client = boto3.client('lexv2-runtime', region_name='ap-southeast-1')'''
+lex_client = boto3.client('lexv2-runtime', region_name='ap-southeast-1')
