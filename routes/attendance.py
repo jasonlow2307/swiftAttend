ddb344983cee6365fe721b602242c254d648f68e
Jason <jason.lowjx@gmail.com>
2024-10-12 11:36:46 +0800

    Final EC2 test

--- routes/attendance.py
+++ routes/attendance.py
@@ -509,7 +509,7 @@ def check_attendance_record():
                 response_search = rekognition.search_faces(
                     CollectionId=REKOGNITION_COLLECTION_NAME,
                     FaceId=face_id,
-                    FaceMatchThreshold=50,
+                    FaceMatchThreshold=20,
                     MaxFaces=50
                 )
 
